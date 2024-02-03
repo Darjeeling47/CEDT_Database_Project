c46faa3f06b7aac3d90c84199076f5ec34ea87bc
witchakra <131113499+witchakra@users.noreply.github.com>
2024-02-04 02:42:48 +0700

    7,8

--- Wit.sql
+++ Wit.sql
@@ -1,5 +1,11 @@
 --6.delete_user(user_id): ลบข้อมูล user ที่ต้องการ ยังไม่เสร็จ
 DELETE FROM users WHERE user_id = 1;
+
 --7.create_restaurant: ลงทะเบียนร้านอาหารใหม่ในระบบ
 INSERT INTO restaurants(restaurant_id,restaurant_owner,restaurant_name,website)
 VALUES (11,'raiden shogun','inazuma' , 'http://www.inazumaRI.com');
+
+--8.edit_restaurant: แก้ไขข้อมูลร้านอาหาร
+UPDATE restaurants
+SET detail = 'seafood'
+WHERE restaurant_id = 11 ;
