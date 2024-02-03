47761da4089b3f44ec4959f5e07a061eecaa2a34
Tippaphanun Kungwarnkanok <tip.kus48@gmail.com>
2024-02-04 00:38:03 +0700

    Update 17, 19-20

--- Gift.sql
+++ Gift.sql
@@ -1,11 +1,13 @@
 -- 17. add_timeslot : เพิ่ม timeslot ของวันนั้น
-INSERT INTO timeslots (timeslot_id, timeslot_date, start_time, end_time)
-VALUE ('2024-02-04', '10:00:00', '10:30:00');
+INSERT INTO timeslots (timeslot_date, start_time, end_time)
+VALUES ('2024-02-04', '10:00:00', '10:30:00');
 
 -- 19. edit_timeslot : แก้ไข timeslot ที่ต้องการ
 UPDATE timeslots
 SET timeslot_date = '2024-02-05', start_time = '13:00:00', end_time = '13:30:00'
-WHERE timeslot_id = 15;
+WHERE timeslot_id = 1;
 
 -- 20. delete_timeslot : ลบ timeslot ที่ต้องการ
-DELETE FROM timeslots WHERE timeslot_id = 15;
+DELETE FROM reserves WHERE timeslot_id = 1;
+DELETE FROM timeslots WHERE timeslot_id = 1;
+
