55d1fd0cd7d368784ed160e1a2dab505e05b0b94
Supawit <supawit.ma@gmail.com>
2024-02-03 10:27:50 +0700

    finished template

---
+++ Template/Function.sql
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- function return variable ------------------------------------------------------
+----------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION name(parameter_1 DATA_TYPE, ...)
+  RETURN DATA_TYPE
+  LANGUAGE plpgsql
+  AS 
+$$
+DECLARE 
+  data_name DATA_TYPE;
+
+BEGIN
+  SELECT * FROM table_name
+  WHERE condition;
+
+  return data_name;
+
+END
+$$
+
+----------------------------------------------------------------------------------
+-- function return table ---------------------------------------------------------
+----------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION name(parameter_1 DATA_TYPE, ...)
+  RETURN TABLE(
+    column_1 DATA_TYPE,
+    column_2 DATA_TYPE
+  )
+  LANGUAGE plpgsql
+  AS 
+$$
+BEGIN
+  SELECT * FROM table_name
+  WHERE condition;
+
+  RETURN qurey;
+
+END
+$$
