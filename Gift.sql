b61ba161fa8d2e48df6322394b979b0811a2794a
Supawit <supawit.ma@gmail.com>
2024-02-04 09:11:37 +0700

    fix something

--- Gift.sql
+++ Gift.sql
@@ -28,10 +28,10 @@ WHERE R.user_id = 1 AND (R.table_id = BT.table_id) AND (BT.branch_id = B.branch_
 -- 30. admin_view_reserves : Admin ดูการจองในจังหวัดที่ตัวเองรับผิดชอบ
 SELECT A.user_id AS admin_id, P.province_name, RS.restaurant_name, B.branch_id, BT.table_number, R.user_id, R.preferred_name, R.amount
 FROM admins A, provinces P, restaurants RS, branches B, branch_tables BT, reserves R
-WHERE A.user_id = 3 AND (A.province_id = P.province_id) AND (P.province_id = B.province_id) AND (B.restaurant_id = RS.restaurant_id) AND (B.branch_id = BT.branch_id) AND (BT.table_id = R.table_id);
+WHERE A.user_id = 1 AND (A.province_id = P.province_id) AND (P.province_id = B.province_id) AND (B.restaurant_id = RS.restaurant_id) AND (B.branch_id = BT.branch_id) AND (BT.table_id = R.table_id);
 
 -- 31. restaurant_manager_view_reserves : Restaurant manager ดูการจองในร้านที่รับผิดชอบ 
 SELECT RM.user_id AS restaurant_manager_id, RS.restaurant_name, B.branch_id, BT.table_number, R.user_id, R.preferred_name, R.amount
 FROM restaurant_managers RM, restaurants RS, branches B, branch_tables BT, reserves R
-WHERE RM.user_id = 4 AND (RM.restaurant_id = RS.restaurant_id) AND (RS.restaurant_id = B.restaurant_id) AND (B.branch_id = BT.branch_id) AND (BT.table_id = R.table_id);
+WHERE RM.user_id = 1 AND (RM.restaurant_id = RS.restaurant_id) AND (RS.restaurant_id = B.restaurant_id) AND (B.branch_id = BT.branch_id) AND (BT.table_id = R.table_id);
 
